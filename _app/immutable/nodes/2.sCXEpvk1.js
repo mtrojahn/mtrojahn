adeb339117e79f431c18ecf28f5b0b8f4b9bdcda
Marcelus Trojahn <mtrojahn@gmail.com>
2023-12-18 19:52:48 -0300

    Updates

---
+++ _app/immutable/nodes/2.sCXEpvk1.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/scheduler.k-kUyWhY.js";import{S as t,i as e}from"../chunks/index.fwz0P8IJ.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
