0bdec2194acad6ea2ee75141801b4b7bf3895dcd
Marcelus Trojahn <mtrojahn@gmail.com>
2023-12-19 15:56:49 -0300

    configurations

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-static'
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from "@sveltejs/adapter-static"
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +11,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/mtrojahn' : ''
+			base: process.env.NODE_ENV === "production" ? "/mtrojahn" : ""
 		}
 	}
 }
