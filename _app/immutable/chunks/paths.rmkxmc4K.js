2bc0fe3142d2b9ada76f2ebb4bab03747451ecc2
Marcelus Trojahn <mtrojahn@gmail.com>
2023-12-21 21:51:59 -0300

    Updates

---
+++ _app/immutable/chunks/paths.rmkxmc4K.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_uslzhs?.base??"/mtrojahn",a=globalThis.__sveltekit_uslzhs?.assets??s;export{a,s as b};
--- _app/immutable/chunks/paths.xKcgwZ2p.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_e7wc1j?.base??"/mtrojahn",e=globalThis.__sveltekit_e7wc1j?.assets??s;export{e as a,s as b};
