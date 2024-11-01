3b5befb4b51580e9ce8a1bc82dc2bf9e238d2a2a
Marcelus Trojahn <mtrojahn@gmail.com>
2024-11-01 15:30:25 +0100

    Updates

--- _app/immutable/chunks/paths.C_r8B_4Q.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_18l5aij)==null?void 0:s.base)??"/mtrojahn";var a;const e=((a=globalThis.__sveltekit_18l5aij)==null?void 0:a.assets)??t;export{e as a,t as b};
--- _app/immutable/entry/start.XwCizpsq.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.BVn3Fss6.js";export{t as start};
