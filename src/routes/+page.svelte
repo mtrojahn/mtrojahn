c86f6f8242035aad74136655f5bbb889604d793a
Marcelus Trojahn <mtrojahn@gmail.com>
2026-02-01 14:23:18 +0100

    update hrefs to use base path for tools

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
+<script>
+	import { base } from "$app/paths"
+</script>
 <div class="container">
 	<h6>Random Tools</h6>
 	<ul>
-		<li><a href="/clockify-export/main">Clockify Export Tool</a></li>
-		<li><a href="/gaming-tasks-app/main">Gaming Tasks App</a></li>
+		<li><a href="{base}/clockify-export/main">Clockify Export Tool</a></li>
+		<li><a href="{base}/gaming-tasks-app/main">Gaming Tasks App</a></li>
 	</ul>
 </div>
