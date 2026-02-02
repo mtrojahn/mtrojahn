84c6574a85ebb66ba14a2b7c3d37df7787b3a92a
Marcelus Trojahn <mtrojahn@gmail.com>
2026-02-02 21:58:24 +0100

    Updates

--- _app/immutable/chunks/Bnj-xq9n.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_dyvrw9)==null?void 0:s.base)??"/mtrojahn";var a;const e=((a=globalThis.__sveltekit_dyvrw9)==null?void 0:a.assets)??t??"";export{e as a,t as b};
